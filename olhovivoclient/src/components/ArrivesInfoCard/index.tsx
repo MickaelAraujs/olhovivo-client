fc62efeb17e6fdf6eeec3ee9d01f26b4571982b2
Mickael Araujo <araujomickael18@gmail.com>
2020-08-18 04:27:25 -0300

    :sparkles: exibindo previsão de chegada baseado em uma parada

--- olhovivoclient/src/components/ArrivesInfoCard/index.tsx
+++ olhovivoclient/src/components/ArrivesInfoCard/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-indent */
 
 import React from 'react';
@@ -5,35 +6,73 @@ import { FiArrowRight } from 'react-icons/fi';
 
 import './styles.css';
 
-const ArrivesInfoCard: React.FC = () => {
+interface ArrivesInfoCardProps {
+    routeData: {
+        lt0: string;
+        lt1: string;
+        totalVehicles: number;
+        routeDirection: number;
+    };
+
+    vehicleData: {
+        letters: string;
+        accessible: boolean;
+        arrivePreview: string;
+    };
+}
+
+const ArrivesInfoCard: React.FC<ArrivesInfoCardProps> = ({
+    routeData,
+    vehicleData,
+}) => {
     return (
         <div className="arrive-card">
             <header className="card-header">
-                <h2 className="card-title">73651</h2>
+                <h2 className="card-title">{vehicleData.letters}</h2>
 
                 <div className="card-info">
-                    <div className="card-info-content">
-                        <span>Origem</span>
-                        <h3>PÇA. RAMOS DE AZEVEDO</h3>
-                    </div>
+                    {routeData.routeDirection === 1 ? (
+                        <>
+                            <div className="card-info-content">
+                                <span>Origem</span>
+                                <h3>{routeData.lt0}</h3>
+                            </div>
 
-                    <FiArrowRight />
+                            <FiArrowRight />
 
-                    <div className="card-info-content">
-                        <span>Destino:</span>
-                        <h3>TERM. LAPA</h3>
-                    </div>
+                            <div className="card-info-content">
+                                <span>Destino:</span>
+                                <h3>{routeData.lt1}</h3>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="card-info-content">
+                                <span>Origem</span>
+                                <h3>{routeData.lt1}</h3>
+                            </div>
+
+                            <FiArrowRight />
+
+                            <div className="card-info-content">
+                                <span>Destino:</span>
+                                <h3>{routeData.lt0}</h3>
+                            </div>
+                        </>
+                    )}
                 </div>
             </header>
 
             <main className="card-content">
                 <p>
                     Previsão de chegada:
-                    <span>23:22</span>
+                    <span>{vehicleData.arrivePreview}</span>
                 </p>
 
                 <footer>
-                    <span>* Acessível a pessoas com deficiência</span>
+                    {vehicleData.accessible && (
+                        <span>* Acessível a pessoas com deficiência</span>
+                    )}
                 </footer>
             </main>
         </div>
--- olhovivoclient/src/pages/Arrives/index.tsx
+++ olhovivoclient/src/pages/Arrives/index.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import Sidebar from '../../components/Sidebar';
 import SearchInput from '../../components/SearchInput';
@@ -10,8 +11,152 @@ import Refresh from '../../components/Refresh';
 import ArrivesInfoCard from '../../components/ArrivesInfoCard';
 
 import './styles.css';
+import authenticate from '../../services/auth';
+import api from '../../services/api';
+import NoSearch from '../../components/NoSearch';
+
+interface StopProps {
+    cp: number;
+}
+
+interface RouteProps {
+    cl: number;
+    sl: number;
+    lt0: string;
+    lt1: string;
+    qv: number;
+    vs: {
+        p: string;
+        t: string;
+        a: boolean;
+    }[];
+}
+
+interface VehicleProps {
+    letters: string;
+    accessible: boolean;
+    arrivePreview: string;
+}
+
+interface RouteDataProps {
+    routeCode: number;
+    lt0: string;
+    lt1: string;
+    totalVehicles: number;
+    routeDirection: number;
+    vehicles: VehicleProps[];
+}
+
+export interface stopsArriveDataProps {
+    cp: number;
+    stopName: string;
+    routesData: RouteDataProps[];
+}
 
 const Arrives: React.FC = () => {
+    const [time, setTime] = useState('');
+    const [stopsArriveData, setStopsArriveData] = useState<
+        stopsArriveDataProps[]
+    >([]);
+
+    const [searchInput, setSearchInput] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function handleSearchArrivesPreview() {
+        setStopsArriveData([]);
+        setIsLoading(true);
+
+        const authResponse = await authenticate();
+
+        if (authResponse) {
+            const stopsSearchResponse = await api.get('Parada/Buscar', {
+                params: {
+                    termosBusca: searchInput,
+                },
+            });
+
+            // retornando todos os códigos de parada em um array
+            const stopsCodes = stopsSearchResponse.data.map(
+                (stop: StopProps) => stop.cp,
+            );
+
+            stopsCodes.forEach(async (stopCode: number) => {
+                const arrivesPreviewResponse = await api.get(
+                    'Previsao/Parada',
+                    {
+                        params: {
+                            codigoParada: stopCode,
+                        },
+                    },
+                );
+
+                const { hr, p: stops } = arrivesPreviewResponse.data;
+
+                if (!stops) {
+                    return;
+                }
+
+                const { cp, np: stopName, l: routes } = stops;
+
+                const routesData = routes.map((route: RouteProps) => {
+                    const { cl, lt0, lt1, qv, sl, vs } = route;
+
+                    const vehicles = vs.map(vehicle => {
+                        const { p, a, t } = vehicle;
+
+                        return {
+                            letters: p,
+                            accessible: a,
+                            arrivePreview: t,
+                        };
+                    });
+
+                    return {
+                        routeCode: cl,
+                        lt0,
+                        lt1,
+                        totalVehicles: qv,
+                        routeDirection: sl,
+                        vehicles,
+                    };
+                });
+
+                setTime(hr);
+                setStopsArriveData(data => [
+                    ...data,
+                    {
+                        cp,
+                        stopName,
+                        routesData,
+                    },
+                ]);
+            });
+        }
+
+        setIsLoading(false);
+    }
+
+    function handleLoadArrivesData(routesData: RouteDataProps[]) {
+        return routesData.map(route => {
+            const { lt0, lt1, totalVehicles, routeDirection } = route;
+
+            const routeData = {
+                lt0,
+                lt1,
+                totalVehicles,
+                routeDirection,
+            };
+
+            return route.vehicles.map((vehicle: VehicleProps, index) => (
+                <ArrivesInfoCard
+                    key={index}
+                    routeData={routeData}
+                    vehicleData={vehicle}
+                />
+            ));
+        });
+    }
+
     return (
         <div className="container">
             <Sidebar />
@@ -25,24 +170,38 @@ const Arrives: React.FC = () => {
                             label="Ponto de parada"
                             name="arrives-preview"
                             placeholder="informe o nome ou endereço da parada"
-                            searchSubmit={() => {}}
+                            value={searchInput}
+                            onChange={e => setSearchInput(e.target.value)}
+                            buttonText={isLoading ? 'Buscando...' : ''}
+                            searchSubmit={handleSearchArrivesPreview}
                         />
                     </form>
                 </header>
 
                 <main className="arrive-page-content">
-                    <Refresh refresh={() => {}} />
+                    {stopsArriveData.length === 0 ? (
+                        <NoSearch />
+                    ) : (
+                        <Refresh
+                            refresh={handleSearchArrivesPreview}
+                            reference={time}
+                        />
+                    )}
 
-                    <h2 className="arrive-title">
-                        PARADA ROBERTO SELMI DEI B/C
-                    </h2>
+                    <div className="search-routes-result-container">
+                        {stopsArriveData.map((arriveData, index) => (
+                            <div key={index}>
+                                <h2 className="arrive-title">
+                                    {arriveData.stopName}
+                                </h2>
 
-                    <div className="search-routes-result">
-                        <ArrivesInfoCard />
-
-                        <ArrivesInfoCard />
-
-                        <ArrivesInfoCard />
+                                <div className="search-routes-result">
+                                    {handleLoadArrivesData(
+                                        arriveData.routesData,
+                                    )}
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </main>
             </div>
